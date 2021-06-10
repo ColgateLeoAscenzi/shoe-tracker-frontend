12e6ed6e6b640de75f8774b329685e3e352249dc
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-06-09 23:34:47 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import * as React from 'react';
 
 function App() {
-    console.log("test");
     return (
         <div className="App">
             <header className="App-header">
