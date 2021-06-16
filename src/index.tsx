f34deabf45707f615a140b02b5ab0e5b78cee4c3
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-06-16 15:12:01 -0400

    fix(*): Added pageview tracker for google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import ReactGA from 'react-ga';
 
 const trackingId = "UA-199666489-2";
 ReactGA.initialize(trackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <React.StrictMode>
